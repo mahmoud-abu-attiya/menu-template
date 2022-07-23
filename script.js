44605cfaf34628729f9d32ff80a882c21ef5a801
mahmoud-abu-attiya <mahmoud.abuattiya106@gmail.com>
2022-07-23 17:22:05 +0200

    add some options

--- script.js
+++ script.js
@@ -6,11 +6,39 @@ AOS.init({
   mirror: false,
   disable: false,
 });
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper_cat", {
   spaceBetween: 30,
   slidesPerView: 3,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+var swiper = new Swiper(".mySwiper_card", {
+  spaceBetween: 5,
+  slidesPerView: 2.5,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 997px
+    997: {
+      slidesPerView: 5
+    }
+  },
+});
+var swiper = new Swiper(".mySwiper_items", {
+  spaceBetween: 5,
+  slidesPerView: 1.2,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 997px
+    997: {
+      slidesPerView: 5
+    }
   },
 });
